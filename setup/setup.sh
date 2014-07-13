324fc0387a60e08e1104bedc509a3b5d87d15c52
Christophe Chaudier <chaudier.christophe@gmail.com>
2014-07-13 15:28:50 +0200

    install shellfactory

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,1 +1,2 @@
-
+curl -L https://raw.githubusercontent.com/cchaudier/shellfactory/master/install.sh > /tmp/sf_install.sh && bash /tmp/sf_install.sh -l ; rm -f /tmp/sf_install.sh
+echo 'export SFLIB=$HOME/.shellfactory/current/lib'>>~/.bashrc
