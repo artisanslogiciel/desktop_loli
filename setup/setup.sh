e467e0cb2dde5f3c208edee89958b5deddddedba
Christophe Chaudier <chaudier.christophe@gmail.com>
2014-07-13 15:45:44 +0200

    install palette

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,2 +1,7 @@
 curl -L https://raw.githubusercontent.com/cchaudier/shellfactory/master/install.sh > /tmp/sf_install.sh && bash /tmp/sf_install.sh -l ; rm -f /tmp/sf_install.sh
 echo 'export SFLIB=$HOME/.shellfactory/current/lib'>>~/.bashrc
+mkdir -p $HOME/palette
+curl -L https://raw.githubusercontent.com/artisanslogiciel/palette/master/palette > $HOME/palette/palette
+chmod +x $HOME/palette/palette
+echo 'export PATH=$HOME/palette:$PATH'>>~/.bashrc
+export PATH=$HOME/palette:$PATH
