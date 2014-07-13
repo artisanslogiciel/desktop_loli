bfe53675b6f2af0e4cab2a88d5bbd1b75db7c3c3
Christophe Chaudier <chaudier.christophe@gmail.com>
2014-07-13 16:26:28 +0200

    launch multi-palette

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,7 +1,28 @@
-curl -L https://raw.githubusercontent.com/cchaudier/shellfactory/master/install.sh > /tmp/sf_install.sh && bash /tmp/sf_install.sh -l ; rm -f /tmp/sf_install.sh
-echo 'export SFLIB=$HOME/.shellfactory/current/lib'>>~/.bashrc
-mkdir -p $HOME/palette
-curl -L https://raw.githubusercontent.com/artisanslogiciel/palette/master/palette > $HOME/palette/palette
-chmod +x $HOME/palette/palette
-echo 'export PATH=$HOME/palette:$PATH'>>~/.bashrc
-export PATH=$HOME/palette:$PATH
+init() {
+  curl -L https://raw.githubusercontent.com/cchaudier/shellfactory/master/install.sh > /tmp/sf_install.sh && bash /tmp/sf_install.sh -l ; rm -f /tmp/sf_install.sh
+  echo 'export SFLIB=$HOME/.shellfactory/current/lib'>>~/.bashrc
+  mkdir -p $HOME/palette
+  curl -L https://raw.githubusercontent.com/artisanslogiciel/palette/master/palette > $HOME/palette/palette
+  chmod +x $HOME/palette/palette
+  echo 'export PATH=$HOME/palette:$PATH'>>~/.bashrc
+  export SFLIB=$HOME/.shellfactory/current/lib
+  export PATH=$HOME/palette:$PATH
+}
+
+palettiser() {
+cd buntufiles
+for file in $(find . -name Buntufile|sort); do
+  trt_palette $file
+done
+}
+
+trt_palette() {
+  nom_palette=$(dirname $file)
+  echo "traite palette $nom_palette"
+  cd $nom_palette
+  palette
+  cd -
+}
+
+init
+palettiser
